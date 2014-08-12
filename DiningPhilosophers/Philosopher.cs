55c67191d0614aef178d6955d0fdb09c1c437e0c
vitaliy <vitaliy@dalet>
2014-08-11 22:12:37 -0700

    first commit

---
+++ DiningPhilosophers/Philosopher.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiningPhilosophers
+{
+    /// <summary>
+    /// Philosopher states
+    /// </summary>
+    public enum PhilosopherState
+    {
+        Eating,
+        Thinking,
+        Done
+    }
+
+
+    /// <summary>
+    /// Philosopher model in the Dining Philosophers problem
+    /// </summary>
+    public class Philosopher
+    {
+        private int ThinkMs;
+        private int EatMs;
+        private int Id;
+        private int FoodCount;
+        private Mutex LeftFork;
+        private Mutex RightFork;
+
+        // wait time for mutex
+        public int MutexWaitMs = 1000;
+
+
+        public PhilosopherState State;
+
+
+        /// <summary>
+        /// Construct a philosopher
+        /// </summary>
+        /// <param name="id">indentifier</param>
+        /// <param name="left">left fork</param>
+        /// <param name="right">right fork</param>
+        /// <param name="thinkMs">time it takes to think</param>
+        /// <param name="eatMs">time it takes to eat</param>
+        /// <param name="food">food units</param>
+        public Philosopher(int id, Mutex left, Mutex right, 
+                           int thinkMs = 2000, int eatMs = 1000, int food = 3)
+        {
+            LeftFork = left;
+            RightFork = right;
+            ThinkMs = thinkMs;
+            EatMs = eatMs;
+            FoodCount = food;
+            Id = id;
+            State = PhilosopherState.Thinking;
+        }
+
+
+        /// <summary>
+        /// Attempt to secure left and right fork and eat food
+        /// </summary>
+        public void Eat()
+        {
+            while (State != PhilosopherState.Done)
+            {
+                //Console.WriteLine("{0} is requesting left", Id);
+                if (LeftFork.WaitOne(MutexWaitMs))
+                {
+                    //Console.WriteLine("{0} is requesting right", Id);
+                    if (RightFork.WaitOne(MutexWaitMs))
+                    {
+                        FoodCount--;
+                        State = PhilosopherState.Eating;
+                        Console.WriteLine("{0} eating; {1} food", Id, FoodCount);
+                        Thread.Sleep(EatMs);
+
+                        // release forks after eating
+                        RightFork.ReleaseMutex();
+                        LeftFork.ReleaseMutex();
+
+                        if (FoodCount < 1)
+                        {
+                            State = PhilosopherState.Done;
+                            Console.WriteLine("{0} done.", Id);
+                        }
+
+                        // commence thinking!
+                        Think();
+                    }
+                    else
+                    {
+                        // release left fork if unable aquire right fork
+                        LeftFork.ReleaseMutex();
+                    }
+                }
+            }
+        }
+
+        
+        /// <summary>
+        /// If still have food, think for awhile
+        /// </summary>
+        public void Think()
+        {
+            if (State == PhilosopherState.Done)
+                return;
+
+            State = PhilosopherState.Thinking;
+            Console.WriteLine("{0} thinking; {1} food", Id, FoodCount);
+            Thread.Sleep(ThinkMs);
+        }
+    }
+}
---
+++ DiningPhilosophers/Table.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace DiningPhilosophers
+{
+    /// <summary>
+    /// Using Mutexes as forks and Philosophers as threads, 
+    /// model the Dining philosophers problem
+    /// </summary>
+    public class PhilosophersTable
+    {
+        public List<Mutex> forks;
+        public List<Thread> threads;
+
+        /// <summary>
+        /// Build philosopher threads
+        /// </summary>
+        /// <param name="party_size">number of philosophers at the table</param>
+        public PhilosophersTable(int party_size)
+        {
+            forks = new List<Mutex>();
+            threads = new List<Thread>();
+
+            // build first fork
+            var left = new Mutex();
+            forks.Add(left);
+
+            // build philosophers and more forks
+            for (int i = 1; i < party_size; i++)
+            {                
+                var right = new Mutex();
+                forks.Add(right);
+                threads.Add(new Thread(new Philosopher(i, left, right).Eat));
+                left = right;
+            }
+
+            // build last philosopher with fork shared by first philosopher
+            var last = new Philosopher(party_size, left, forks.First());
+            threads.Add(new Thread(last.Eat));
+        }
+
+
+        /// <summary>
+        /// Start threads
+        /// </summary>
+        public void Run()
+        {
+            Console.WriteLine("Philosophers {0}  Forks {0}", 
+                threads.Count(), forks.Count());
+
+            foreach (var t in threads)
+            {
+                t.Start();
+            }
+        }
+    }
+}
+
