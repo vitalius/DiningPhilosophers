583783b9ad5d0c42ef2c09d5a64394de452458da
vitaliy <vitaliy@dalet>
2014-08-17 11:39:47 -0700

    now with WPF

--- DiningPhilosophers/Philosopher.cs
+++ DiningPhilosophers/Philosopher.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Security.Permissions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DiningPhilosophers
 {
     /// <summary>
-    /// Philosopher states
+    /// Different philosophers' states
     /// </summary>
     public enum PhilosopherState
     {
+        Hungry,
         Eating,
         Thinking,
         Done
@@ -18,74 +24,85 @@ namespace DiningPhilosophers
     /// <summary>
     /// Philosopher model in the Dining Philosophers problem
     /// </summary>
-    public class Philosopher
+    internal class Philosopher
     {
         private int ThinkMs;
         private int EatMs;
-        private int Id;
         private int FoodCount;
+
         private Mutex LeftFork;
         private Mutex RightFork;
 
-        // wait time for mutex
-        public int MutexWaitMs = 1000;
+        private Ellipse pSprite, lSprite, rSprite;
+        private Label statusLabel;
+        private TextBox foodText;
 
-
-        public PhilosopherState State;
+        private PhilosopherState State;
 
 
         /// <summary>
-        /// Construct a philosopher
+        /// Build philosopher thread
         /// </summary>
-        /// <param name="id">indentifier</param>
-        /// <param name="left">left fork</param>
-        /// <param name="right">right fork</param>
-        /// <param name="thinkMs">time it takes to think</param>
-        /// <param name="eatMs">time it takes to eat</param>
-        /// <param name="food">food units</param>
-        public Philosopher(int id, Mutex left, Mutex right, 
-                           int thinkMs = 2000, int eatMs = 1000, int food = 3)
+        /// <param name="id">identifier, helps with random number generator</param>
+        /// <param name="left">left Fork mutex</param>
+        /// <param name="right">right Fork mutex</param>
+        /// <param name="philSprite">UI ellipse for marking philosopher</param>
+        /// <param name="lforkSprite">UI ellipse for left fork</param>
+        /// <param name="rforkSprite">UI ellipse for right fork</param>
+        /// <param name="pLabel">UI label for showing philosopher status</param>
+        /// <param name="fText">UI TextBox for food counter</param>
+        public Philosopher(int id, Mutex left, Mutex right,
+                           Ellipse philSprite,
+                           Ellipse lforkSprite, Ellipse rforkSprite,
+                           Label pLabel, TextBox fText)
         {
             LeftFork = left;
             RightFork = right;
-            ThinkMs = thinkMs;
-            EatMs = eatMs;
-            FoodCount = food;
-            Id = id;
-            State = PhilosopherState.Thinking;
+
+            // activity timers
+            Random rnd = new Random((int)DateTime.Now.Ticks + id);
+            ThinkMs = rnd.Next(100, 500);
+            EatMs = rnd.Next(100, 500);
+            FoodCount = rnd.Next(10, 30);
+
+            // ui
+            pSprite = philSprite;
+            lSprite = lforkSprite;
+            rSprite = rforkSprite;
+            foodText = fText;
+            statusLabel = pLabel;            
         }
 
 
         /// <summary>
-        /// Attempt to secure left and right fork and eat food
+        /// Secure left and right fork and commence eating.
         /// </summary>
-        public void Eat()
+        public void Participate()
         {
+            SetState(PhilosopherState.Thinking);
+
             while (State != PhilosopherState.Done)
             {
-                //Console.WriteLine("{0} is requesting left", Id);
-                if (LeftFork.WaitOne(MutexWaitMs))
+                // attempt to aquire left fork
+                if (LeftFork.WaitOne(10))
                 {
-                    //Console.WriteLine("{0} is requesting right", Id);
-                    if (RightFork.WaitOne(MutexWaitMs))
+                    // now right fork
+                    if (RightFork.WaitOne(10))
                     {
-                        FoodCount--;
-                        State = PhilosopherState.Eating;
-                        Console.WriteLine("{0} eating; {1} food left", Id, FoodCount);
-                        Thread.Sleep(EatMs);
+                        // clean the forks ;)
+                        Thread.Sleep(100);
 
-                        // release forks after eating
+                        SetState(PhilosopherState.Eating);
+
                         RightFork.ReleaseMutex();
                         LeftFork.ReleaseMutex();
 
-                        if (FoodCount < 1)
-                        {
-                            State = PhilosopherState.Done;
-                            Console.WriteLine("{0} done.", Id);
-                        }
+                        SetState(PhilosopherState.Thinking);
 
-                        // commence thinking!
-                        Think();
+                        if (FoodCount > 0)
+                            SetState(PhilosopherState.Hungry);
+                        else
+                            SetState(PhilosopherState.Done);
                     }
                     else
                     {
@@ -96,18 +113,44 @@ namespace DiningPhilosophers
             }
         }
 
-        
-        /// <summary>
-        /// If still have food, think for awhile
-        /// </summary>
-        public void Think()
-        {
-            if (State == PhilosopherState.Done)
-                return;
 
-            State = PhilosopherState.Thinking;
-            Console.WriteLine("{0} thinking", Id);
-            Thread.Sleep(ThinkMs);
+        /// <summary>
+        /// Update philosoher state and corresponding UI elements
+        /// </summary>
+        public void SetState(PhilosopherState state)
+        {
+            State = state;
+
+            switch (State)
+            {
+                case PhilosopherState.Hungry:
+                    statusLabel.Dispatcher.Invoke(new Action(() => statusLabel.Content = "Hungry"));
+                    foodText.Dispatcher.Invoke(new Action(() => foodText.Text = FoodCount.ToString()));
+                    break;
+                case PhilosopherState.Eating:
+                    rSprite.Dispatcher.Invoke(new Action(() => rSprite.Visibility = Visibility.Visible));
+                    lSprite.Dispatcher.Invoke(new Action(() => lSprite.Visibility = Visibility.Visible));
+                    pSprite.Dispatcher.Invoke(new Action(() => pSprite.Visibility = Visibility.Visible));
+                    statusLabel.Dispatcher.Invoke(new Action(() => statusLabel.Content = "Eating"));
+                    foodText.Dispatcher.Invoke(new Action(() => foodText.Text = FoodCount.ToString()));
+                    FoodCount--;
+                    Thread.Sleep(EatMs);
+                    break;
+                case PhilosopherState.Thinking:
+                    rSprite.Dispatcher.Invoke(new Action(() => rSprite.Visibility = Visibility.Hidden));
+                    lSprite.Dispatcher.Invoke(new Action(() => lSprite.Visibility = Visibility.Hidden));
+                    pSprite.Dispatcher.Invoke(new Action(() => pSprite.Visibility = Visibility.Hidden));
+                    statusLabel.Dispatcher.Invoke(new Action(() => statusLabel.Content = "Thinking..."));
+                    foodText.Dispatcher.Invoke(new Action(() => foodText.Text = FoodCount.ToString()));
+                    Thread.Sleep(ThinkMs);
+                    break;
+                case PhilosopherState.Done:
+                    statusLabel.Dispatcher.Invoke(new Action(() => statusLabel.Content = "Done."));
+                    foodText.Dispatcher.Invoke(new Action(() => foodText.Text = FoodCount.ToString()));
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
