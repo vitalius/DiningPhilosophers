f9db6e8df6c4ffa78715850ab8a01d3030f762b5
vitaliy <vitaliy@dalet>
2014-08-17 11:41:07 -0700

    adding WPF files

---
+++ DiningPhilosophers/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiningPhilosophers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<Ellipse> philosophers = new List<Ellipse>();
+        public static List<Label> philLabels = new List<Label>();
+        public static List<Ellipse> forks = new List<Ellipse>();
+        public static List<TextBox> foodText = new List<TextBox>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            philosophers.Add(LayoutRoot.FindName("Philosopher1") as Ellipse);
+            philosophers.Add(LayoutRoot.FindName("Philosopher2") as Ellipse);
+            philosophers.Add(LayoutRoot.FindName("Philosopher3") as Ellipse);
+            philosophers.Add(LayoutRoot.FindName("Philosopher4") as Ellipse);
+            philosophers.Add(LayoutRoot.FindName("Philosopher5") as Ellipse);
+
+            forks.Add(LayoutRoot.FindName("Fork1") as Ellipse);
+            forks.Add(LayoutRoot.FindName("Fork2") as Ellipse);
+            forks.Add(LayoutRoot.FindName("Fork3") as Ellipse);
+            forks.Add(LayoutRoot.FindName("Fork4") as Ellipse);
+            forks.Add(LayoutRoot.FindName("Fork5") as Ellipse);
+
+            philLabels.Add(LayoutRoot.FindName("PhilosopherFood1") as Label);
+            philLabels.Add(LayoutRoot.FindName("PhilosopherFood2") as Label);
+            philLabels.Add(LayoutRoot.FindName("PhilosopherFood3") as Label);
+            philLabels.Add(LayoutRoot.FindName("PhilosopherFood4") as Label);
+            philLabels.Add(LayoutRoot.FindName("PhilosopherFood5") as Label);
+
+            foodText.Add(LayoutRoot.FindName("FoodText1") as TextBox);
+            foodText.Add(LayoutRoot.FindName("FoodText2") as TextBox);
+            foodText.Add(LayoutRoot.FindName("FoodText3") as TextBox);
+            foodText.Add(LayoutRoot.FindName("FoodText4") as TextBox);
+            foodText.Add(LayoutRoot.FindName("FoodText5") as TextBox);
+        }
+
+        private void Run_Click(object sender, RoutedEventArgs e)
+        {
+            var t = new PhilosophersTable(philosophers, forks, philLabels, foodText);
+            t.Run();
+        }
+    }
+}
---
+++ DiningPhilosophers/PhilosophersTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace DiningPhilosophers
+{
+    /// <summary>
+    /// Using Mutexes as forks and Philosophers as threads, 
+    /// model the Dining philosophers problem
+    /// </summary>
+    public class PhilosophersTable
+    {
+        public List<Mutex> lstForkMutexs;
+        public List<Thread> lstPhilosopherThreads;
+
+        /// <summary>
+        /// Build philosopher threads
+        /// </summary>
+        public PhilosophersTable(List<Ellipse> pSprites, 
+                                 List<Ellipse> fSprites, 
+                                 List<Label> pLabels,
+                                 List<TextBox> foodTexts)
+        {
+            lstForkMutexs = new List<Mutex>();
+            lstPhilosopherThreads = new List<Thread>();
+
+            // build first fork
+            var leftForkMutex = new Mutex();
+            lstForkMutexs.Add(leftForkMutex);
+
+            // build philosophers and more forks
+            for (int id = 0; id < pSprites.Count() - 1; id++)
+            {
+                var rightForkMutex = new Mutex();
+                lstForkMutexs.Add(rightForkMutex);
+                lstPhilosopherThreads.Add(
+                    new Thread(
+                        new Philosopher(
+                            id, leftForkMutex, rightForkMutex,
+                            pSprites.ElementAt(id),
+                            fSprites.ElementAt(id), fSprites.ElementAt(id + 1),
+                            pLabels.ElementAt(id), foodTexts.ElementAt(id)).Participate));
+                leftForkMutex = rightForkMutex;
+            }
+
+            // build last philosopher, fork shared with first philosopher
+            lstPhilosopherThreads.Add(
+                new Thread(
+                    new Philosopher(
+                        pSprites.Count(), lstForkMutexs.Last(), lstForkMutexs.First(),
+                        pSprites.Last(),
+                        fSprites.Last(), fSprites.First(),
+                        pLabels.Last(), foodTexts.Last()).Participate));
+        }
+
+
+        /// <summary>
+        /// Start philosopher threads
+        /// </summary>
+        public void Run()
+        {
+            foreach (var t in lstPhilosopherThreads)
+            {
+                t.Start();
+            }
+        }
+    }
+}
+
